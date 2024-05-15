72bc0bdf61c1f115783095688028b40a7ff3a5d6
luiz.felipe <luiz.felipe@geotech4web.com.br>
2024-05-15 11:37:16 -0300

    iniciando projeto

---
+++ micro-servico/src/test/java/com/projeto/scribe/MicroServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.scribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
